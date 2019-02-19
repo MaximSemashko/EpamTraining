03c173d6f8fc8f5a4a4b4377153f6149ab841030
MaximSemashko <maxim.semashko98@gmail.com>
2019-02-19 17:22:10 +0300

    added toast for conflict

--- app/src/main/java/com/example/epamtraining/MainActivity.java
+++ app/src/main/java/com/example/epamtraining/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.epamtraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Toast for conflict", Toast.LENGTH_SHORT)
+                .show();
     }
 }
