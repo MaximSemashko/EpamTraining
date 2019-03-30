96c0124926ef75cf41fc438fccc6183aa7c5f912
MaximSemashko <maxim.semashko98@gmail.com>
2019-03-30 22:50:40 +0300

    added model Trainings and simple realisation for it without recycler

---
+++ app/src/main/java/com/example/epamtraining/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.epamtraining
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Merged revisions", Toast.LENGTH_SHORT)
+                .show()
+    }
+}
---
+++ app/src/main/java/com/example/epamtraining/models/Trainings.java
@@ -0,0 +1,12 @@
+package com.example.epamtraining.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trainings {
+    public Map<String,Integer> exercises;
+
+    public Trainings() {
+        this.exercises = new HashMap<String, Integer>();
+    }
+}
