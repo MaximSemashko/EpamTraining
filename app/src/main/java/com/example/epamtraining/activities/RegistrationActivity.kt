4c75cfaff282785521162bd29d48453762600cda
MaximSemashko <maxim.semashko98@gmail.com>
2019-04-19 19:34:30 +0300

    added login/registration activities

---
+++ app/src/main/java/com/example/epamtraining/activities/RegistrationActivity.kt
@@ -0,0 +1,30 @@
+package com.example.epamtraining.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.example.epamtraining.R
+import com.example.epamtraining.models.Users
+import kotlinx.android.synthetic.main.activity_registration.*
+import java.util.*
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+
+        registerButton.setOnClickListener {
+            val user = Users(id = UUID.randomUUID(),
+                    email = usersEmailEditText.text.toString(),
+                    name = usersNameEditText.text.toString(),
+                    password = usersPasswordEditText.text.toString(),
+                    age = Integer.parseInt(usersAgeEditText.text.toString()),
+                    weight = usersWeightEditText.text.toString().toDouble(),
+                    height = usersHeightEditText.text.toString().toDouble())
+
+            Toast.makeText(this, user.toString(),Toast.LENGTH_LONG).show()
+        }
+    }
+}
--- app/src/main/java/com/example/epamtraining/models/Users.kt
+++ app/src/main/java/com/example/epamtraining/models/Users.kt
@@ -2,8 +2,13 @@ package com.example.epamtraining.models
 
 import java.util.*
 
-data class Users(val id: UUID, val email: String,
-                 val password: String, val name: String,
-                 val sex: String, val age: Int,
-                 val weight: Int, val height: Double,
-                 val trainings: List<Trainings>)
+data class Users(val id: UUID,
+                 val email: String,
+                 val password: String,
+                 val name: String,
+                 var photo: String = "",
+                 val sex: String = "male",
+                 var age: Int = 0,
+                 var weight: Double = 0.0,
+                 var height: Double = 0.0
+                 )
