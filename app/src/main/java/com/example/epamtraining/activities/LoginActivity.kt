1ff5bc9793a403f19fa0b80d3562ae3b1519fe82
MaximSemashko <maxim.semashko98@gmail.com>
2019-04-20 23:41:18 +0300

    started doing profile layout

--- app/src/main/java/com/example/epamtraining/activities/LoginActivity.kt
+++ app/src/main/java/com/example/epamtraining/activities/LoginActivity.kt
@@ -11,7 +11,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        sign_in_button.setOnClickListener {
+        signInButton.setOnClickListener {
 
         }
     }
