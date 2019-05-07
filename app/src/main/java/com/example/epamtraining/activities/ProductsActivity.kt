b05cf88a064d07549153cf852b7f482a1c3fc55c
MaximSemashko <maxim.semashko98@gmail.com>
2019-05-07 18:47:10 +0300

    continue to work with products list

--- app/src/main/java/com/example/epamtraining/activities/ProductsActivity.kt
+++ app/src/main/java/com/example/epamtraining/activities/ProductsActivity.kt
@@ -3,6 +3,7 @@ package com.example.epamtraining.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import com.example.epamtraining.R
 import com.example.epamtraining.adapters.ProductsAdapter
 import com.example.epamtraining.backend.IProductsWebService
@@ -14,6 +15,7 @@ import com.squareup.okhttp.Request
 import com.squareup.okhttp.Response
 import kotlinx.android.synthetic.main.activity_products.*
 import java.io.IOException
+
 
 class ProductsActivity : AppCompatActivity() {
 
@@ -32,30 +34,51 @@ class ProductsActivity : AppCompatActivity() {
         }
 
         fetchJson()
-
     }
 
     private fun fetchJson() {
-
         val request = Request.Builder().url(url).build()
-
         val client = OkHttpClient()
         client.newCall(request).enqueue(object : Callback {
             override fun onResponse(response: Response?) {
                 val responseBody = response?.body()?.string()
-                println(responseBody)
-
+                Log.i("TAG",responseBody)
                 val gson = GsonBuilder().create()
                 val products = gson.fromJson(responseBody, Products::class.java)
-                runOnUiThread {
+                Log.i("TAG",products.toString())
+                runOnUiThread{
                     productsAdapter.updateItems(listOf(products))
-
                 }
             }
 
             override fun onFailure(request: Request?, e: IOException?) {
-                println("Failed to fetch JSON from URL")
             }
         })
     }
+
+    /* private fun fetchJson() {
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(response: Response?) {
+                val responseBody = response?.body()?.string()
+                val gson = GsonBuilder().create()
+                val objects = JSONObject(responseBody)
+                val iterator = objects.keys()
+                while (iterator.hasNext()) {
+                    val obj = objects.getJSONObject(iterator.next())
+                    val prod =gson.from
+                            runOnUiThread {
+                                productsAdapter.updateItems(obj)
+
+                            }
+                    Log.d("TAG", obj.toString())
+                }
+            }
+
+            override fun onFailure(request: Request?, e: IOException?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+    }*/
 }
