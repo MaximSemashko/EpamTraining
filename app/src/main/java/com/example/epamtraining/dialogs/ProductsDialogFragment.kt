c4b706fbf2928f5b8d10793ca91b487a0f74953b
MaximSemashko <maxim.semashko98@gmail.com>
2019-05-09 23:23:52 +0300

    fixed NPE in dialog

--- app/src/main/java/com/example/epamtraining/dialogs/ProductsDialogFragment.kt
+++ app/src/main/java/com/example/epamtraining/dialogs/ProductsDialogFragment.kt
@@ -4,8 +4,8 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.widget.EditText
 import com.example.epamtraining.R
-import kotlinx.android.synthetic.main.products_dialog.*
 
 
 class ProductsDialogFragment : DialogFragment() {
@@ -14,19 +14,22 @@ class ProductsDialogFragment : DialogFragment() {
         fun addProduct(name: String, calories: Double)
     }
 
+    private lateinit var productName: EditText
+    private lateinit var productCalories: EditText
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val context = activity
         val builder = AlertDialog.Builder(context!!)
         val dialogView = activity!!.layoutInflater.inflate(R.layout.products_dialog, null)
+        productName = dialogView.findViewById(R.id.productNameEditText);
+        productCalories = dialogView.findViewById(R.id.productCaloriesEditText);
 
         builder.setView(dialogView)
                 .setPositiveButton("Add") { dialog, id ->
-                    val name: String = productNameEditText?.text.toString()
-                    val calories: Double = productCaloriesEditText?.text.toString().toDouble()
+                    val name: String = productName.text.toString()
+                    val calories: Double = productCalories.text.toString().toDouble()
                     val listener = activity as addProductDialogListener?
-
-                    listener?.addProduct(name,calories)
+                    listener?.addProduct(name, calories)
                 }
                 .setNegativeButton("Cancel") { dialog, id -> dialog.dismiss() }
 
