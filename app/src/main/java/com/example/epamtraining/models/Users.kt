de88d24ee1b7c2bf877cf5aec4762086c1fdb55d
MaximSemashko <maxim.semashko98@gmail.com>
2019-05-15 23:30:52 +0300

    fixed some bugs with auth, retrieved localId

--- app/src/main/java/com/example/epamtraining/models/Users.kt
+++ app/src/main/java/com/example/epamtraining/models/Users.kt
@@ -5,8 +5,8 @@ data class Users(val id: String?,
                  val password: String?,
                  val name: String?,
                  var photo: String = "",
-                 val sex: String = "male",
-                 var age: Int = 0,
-                 var weight: Double = 0.0,
-                 var height: Double = 0.0
+                 val sex: String? = "male",
+                 var age: Int? = 0,
+                 var weight: Double? = 0.0,
+                 var height: Double? = 0.0
                  )
